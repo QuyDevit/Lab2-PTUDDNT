150c02e06b31f4ec96a86853398906eafc134299
QuyDevit <ngocquydev02@gmail.com>
2024-04-28 13:23:19 +0700

    update file

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
+import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {} from '@expo/vector-icons'
+import Contacts from './screen/Contacts';
+import StackNavigator from './navigation/StackNavigator'
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigator from './navigation/TabNavigator';
 
-export default function App() {
+const App =() => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <NavigationContainer>
+        {/* <StackNavigator></StackNavigator> */}
+      <TabNavigator></TabNavigator>
+    </NavigationContainer>
+
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App
